764eed69363751fd3ed2feb8b6a8eddf69c41649
Daniil Soldatenkov <soldatenkov200418@mail.ru>
2023-05-30 23:34:58 +0300

    +func rotate

--- tetris/FileName.cpp
+++ tetris/FileName.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 wstring tetromino[7];
 
+int Rotate(int px, int py, int r) {
+	switch (r%4)
+	{
+	case 0: return py * 4 + px;
+	case 1: return 12 + py - (px * 4);
+	case 2: return 15 - (py * 4) - px;
+	case 3: return 3 - py + (px * 4);
+	}
+}
+
 int main() {
 	tetromino[0].append(L"..X.");
 	tetromino[0].append(L"..X.");
